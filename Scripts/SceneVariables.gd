8e1167caf12e2120544aa32a529def5f600cb16c
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-01-24 17:11:35 +0100

    Scene variables updated

--- Scripts/SceneVariables.gd
+++ Scripts/SceneVariables.gd
@@ -1,8 +1,13 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var initial_paint = 10
+var ball_spawn_rate = 10 #per second
+var barrier_erect_speed = 10 #per second
+var barrier_strength = 10 
+var ball_velocity = 5
+var ball_strength = 4
+var high_score_threshold = 100
+var points_per_ball = 10
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
