3121baa712770a9bbbdaa8f00f042dccb7bb05df
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-01-26 14:52:41 +0100

    Drawing debug circle

--- Scripts/Barrier.gd
+++ Scripts/Barrier.gd
@@ -1,9 +1,34 @@
 extends KinematicBody2D
 
+#for touch calculation
+var outer_circle
+var inner_circle
+
 func _ready():
 
     pass
 
-func __physics_process(delta):
+func _physics_process(delta):
+
+    pass
+
+func draw_empty_circle(circle_center, circle_radius, color, resolution, thick):
+    var draw_counter = 1
+    var line_origin = Vector2()
+    var line_end = Vector2()
+    line_origin = circle_radius + circle_center
+
+    while draw_counter <= 360:
+        line_end = circle_radius.rotated(deg2rad(draw_counter)) + circle_center
+        draw_line(line_origin, line_end, color, thick)
+        draw_counter += 1 / resolution
+        line_origin = line_end
+
+    line_end = circle_radius.rotated(deg2rad(360)) + circle_center
+    draw_line(line_origin, line_end, color, thick)
+	
+
+func _draw():
+    draw_empty_circle(get_node("/root/SceneVariables").center_location, Vector2(get_viewport().size.y * 0.2, get_viewport().size.y * 0.2), Color(1.0, 0.0, 0.0, 0.2), 1, 20)
 
     pass
--- Scripts/SceneVariables.gd
+++ Scripts/SceneVariables.gd
@@ -9,11 +9,10 @@ var ball_strength = 4
 var high_score_threshold = 100
 var points_per_ball = 10
 
-
 var center_location
 
 func _ready():
-	center_location = Vector2(get_viewport().size.x / 2, get_viewport().size.y / 2)
+	center_location = Vector2(get_viewport().size.x / 2.0, get_viewport().size.y / 2.0)
 	print(center_location)
 
 	pass
