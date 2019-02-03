029325567877bb82c3d5dd3a6d8a8ce82b8cafd6
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-02-03 11:05:41 +0100

    Added mothership sprite

---
+++ Scripts/Mothership.gd
@@ -0,0 +1,10 @@
+extends Sprite
+
+func _ready():
+    position = get_node("/root/SceneVariables").center_location
+    pass
+
+#func _process(delta):
+#    # Called every frame. Delta is time since last frame.
+#    # Update game logic here.
+#    pass
