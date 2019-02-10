2927c7750e709416932a00314aa185bfa06ee1e3
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-02-10 13:11:43 +0100

    Menu flow update

---
+++ Scripts/UpgradeMenu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var main_menu_button
+
+func _ready():
+    main_menu_button = get_tree().get_root().get_node("UpgradeMenu/MainMenuButton")
+
+func _process(delta):
+    if main_menu_button.pressed:
+        get_tree().change_scene("res://Scenes/MainMenu.tscn")
