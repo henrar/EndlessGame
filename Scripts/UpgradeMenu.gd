0280f7e86b0defa31adc86a6824e05ea079c726e
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-02-15 17:52:30 +0100

    UpgradeMenu start button

--- Scripts/UpgradeMenu.gd
+++ Scripts/UpgradeMenu.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+var start_button
 var main_menu_button
 
 func _ready():
+    start_button = get_tree().get_root().get_node("UpgradeMenu/StartButton")
     main_menu_button = get_tree().get_root().get_node("UpgradeMenu/MainMenuButton")
 
 func _process(delta):
+    if start_button.pressed:
+        get_tree().change_scene("res://Scenes/Game.tscn")
+
     if main_menu_button.pressed:
         get_tree().change_scene("res://Scenes/MainMenu.tscn")
         
