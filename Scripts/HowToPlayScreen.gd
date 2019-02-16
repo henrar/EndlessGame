980ed05b7fbc324d65ca8168b7874eb093a5f25b
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-02-16 22:39:30 +0100

    Updated HowToPlay screen

--- Scripts/HowToPlayScreen.gd
+++ Scripts/HowToPlayScreen.gd
@@ -2,16 +2,22 @@ extends Node2D
 
 var main_menu_button
 
+var how_to_play_sprite
+
 var background_sprite
 
 onready var scene_variables = get_node("/root/SceneVariables")
 
 func _ready():
     main_menu_button = get_tree().get_root().get_node("HowToPlayScreen/MainMenuButton")
+    how_to_play_sprite = get_tree().get_root().get_node("HowToPlayScreen/HowToPlaySprite")
     background_sprite = get_tree().get_root().get_node("HowToPlayScreen/Background")
 
     main_menu_button.rect_scale *= scene_variables.scale_factor
     main_menu_button.rect_position *= scene_variables.scale_factor
+
+    how_to_play_sprite.scale *= scene_variables.scale_factor
+    how_to_play_sprite.global_position *= scene_variables.scale_factor
 
     background_sprite.scale *= scene_variables.scale_factor
     background_sprite.global_position *= scene_variables.scale_factor
