17ea5123151ef868e5314d709d4a24be22ca4f91
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-02-17 11:40:56 +0100

    Updated gitignore

--- Scripts/UpgradeMenu.gd
+++ Scripts/UpgradeMenu.gd
@@ -2,15 +2,20 @@ extends Node2D
 
 var start_button
 var main_menu_button
+var upgrade_window
 
 var background_sprite
 onready var scene_variables = get_node("/root/SceneVariables")
 
 func _ready():
+    upgrade_window = get_tree().get_root().get_node("UpgradeMenu/UpgradeWindow")
     start_button = get_tree().get_root().get_node("UpgradeMenu/StartButton")
     main_menu_button = get_tree().get_root().get_node("UpgradeMenu/MainMenuButton")
-
     background_sprite = get_tree().get_root().get_node("UpgradeMenu/Background")
+
+    upgrade_window.scale *= scene_variables.scale_factor
+    upgrade_window.global_position *= scene_variables.scale_factor
+
     background_sprite.scale *= scene_variables.scale_factor
     background_sprite.global_position *= scene_variables.scale_factor
 
